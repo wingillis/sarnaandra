7fb91295cef1bc857d989f5ef4f0c3ae5be6d1a1
Winthrop Gillis <win.gillis@gmail.com>
2015-03-29 14:28:33 -0400

    Added some features and made osx file system work

--- helpers.py
+++ helpers.py
@@ -9,7 +9,16 @@ import sys
 class DiskSpace(object):
     def __init__(self):
         self.spaceAvailable = 0
+        self.totalSpace = 0
         self.updated = False
+
+    def update_space(self, free_space, totalSpace):
+        self.spaceAvailable = free_space
+        self.totalSpace = totalSpace
+        self.updated = True
+
+    def get_space(self):
+        return (self.spaceAvailable, self.totalSpace)
 
 mainDisk = DiskSpace()
 
@@ -22,11 +31,12 @@ def get_free_disk_space(path):
             free_bytes = ctypes.c_ulonglong(0)
             total = ctypes.c_ulonglong(0)
             ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(path),
-                                                       None, ctypes.pointer(total), ctypes.pointer(free_bytes))
-            return (free_bytes.value/1024/1024/1024, total.value/1024/1024/1024)
+                None, ctypes.pointer(total), ctypes.pointer(free_bytes))
+            mainDisk.update_space(free_bytes.value/1024/1024/1024, total.value/1024/1024/1024)
+            return mainDisk.get_space()
         else:
             st = os.statvfs(path)
-            return ((st.f_bavail.value * st.f_frsize.value)/1024/1024/1024, (st.f_blocks.value * st.f_frsize.value)/1024/1024/1024)
+            mainDisk.update_space((st.f_bavail * st.f_frsize)/1024.0/1024/1024, (st.f_blocks * st.f_frsize)/1024.0/1024/1024)
+            return mainDisk.get_space()
     else:
-        return mainDisk.spaceAvailable
-
+        return mainDisk.get_space()
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from flask import Flask, request, url_for, render_template,redirect
 import data_database as db
 import platform
 import helpers
+import os
 
 if platform.system() == 'Windows':
     default_drive_path = 'C:\\'
 else:
-    default_drive_path = '~'
+    default_drive_path = os.path.expanduser('~')
 
 # create an instance of the app
 app = Flask(__name__)
@@ -20,7 +21,7 @@ def main():
     exps = base.get_all_experiments()
     (space, total) = helpers.get_free_disk_space(default_drive_path)
     kwargs = {'experiments': exps, 'freespace': round(space, 2), 'totalspace': round(total, 2),
-              'percentage': round(space/total*100, 2), 'drive': default_drive_path}
+              'percentage': round(float(space)/total*100,2), 'drive': default_drive_path}
     return render_template('exp_lists.html', **kwargs)
 
 @app.route("/add_experiment", methods=['post', 'get'])
