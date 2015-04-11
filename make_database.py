7b870ed667d64d6a11eba5ea6e329ce2af1962fb
wingillis <win.gillis@gmail.com>
2015-04-10 22:27:44 -0400

    Table creation helper

--- make_database.py
+++ make_database.py
@@ -1,17 +1,16 @@
 
 
-def run(db_handle, list_of_tables):
+def run(db_handle):
     '''Runs all the necessary functions to make any databases not in the
     database yet.'''
-    table_execute = {'experiments': '',
-                     'files': '',
-                     'recurring_files': '',
-                     'watched_folders': '',
-                     'settings': ''}
+    table_execute = {'experiments': 'CREATE TABLE experiments(date_begin text, date_end text, data_type text, files text, name text, folder_paths text)',
+                     'files': 'CREATE TABLE files(date_added text, date_modified text, data_type text, filesize real, name text, experiment text, tags text, additional_files text)',
+                     'recurring_files': 'CREATE TABLE recurring_files(id Primary key int, filename text, script_type text, time_interval real)',
+                     'watched_folders': "CREATE TABLE watched_folders(id int primary key, path text, experiment text, data_type text)",
+                     'settings': 'CREATE TABLE settings'}
     tables = db_handle.get_tables()
     for key in table_execute.keys():
         if key not in tables:
-    		db_handle.add_table(table_execute[key])
+            db_handle.add_table(table_execute[key])
 
     db_handle.save()
-
